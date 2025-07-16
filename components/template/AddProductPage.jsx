d1afb9386f5036f8cb2406c3fa9118600006e6e8
mmd-yavarii <mdyavarii@gmail.com>
2025-07-16 19:59:06 +0330

    create form for add new products and handle it

---
+++ components/template/AddProductPage.jsx
@@ -0,0 +1,98 @@
+import { categories } from '@/helper/varables';
+import styles from './AddProductPage.module.css';
+import { useState } from 'react';
+
+export default function AddProductPage({ handler }) {
+  const [imgCounter, setImgCounter] = useState(1);
+
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    category: '',
+    images: [],
+    qty: 1,
+    price: 0,
+    discount: 0,
+  });
+
+  function changeHandler(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    if (name === 'images') {
+      const file = event.target.files[0];
+      if (file) {
+        setForm((prev) => ({ ...prev, images: [...prev.images, file] }));
+      }
+      return;
+    }
+    setForm((prev) => ({ ...prev, [name]: value }));
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div>
+          <label htmlFor="title" className={styles.required}>
+            نام محصول
+          </label>
+          <input type="text" id="title" name="title" onChange={changeHandler} value={form.title} />
+        </div>
+
+        <div>
+          <label htmlFor="description" className={styles.required}>
+            توضیحات محصول
+          </label>
+          <input type="text" id="description" name="description" onChange={changeHandler} value={form.description} />
+        </div>
+
+        <div>
+          <label htmlFor="qty">تعداد موجودی</label>
+          <input type="number" id="qty" name="qty" min="0" onChange={changeHandler} value={form.qty} />
+        </div>
+
+        <div>
+          <label htmlFor="price" className={styles.required}>
+            قیمت محصول (تومان)
+          </label>
+          <input type="number" id="price" name="price" min="0" onChange={changeHandler} value={form.price} />
+        </div>
+
+        <div>
+          <label htmlFor="discount">درصد تخفیف (اختیاری)</label>
+          <input type="number" id="discount" name="discount" min="0" max="100" onChange={changeHandler} value={form.discount} />
+        </div>
+
+        <div>
+          <label htmlFor="category" className={styles.required}>
+            دسته‌بندی محصول
+          </label>
+          <select id="category" name="category" onChange={changeHandler} value={form.category}>
+            <option value="">انتخاب دسته‌بندی محصول</option>
+            {categories.map((i) => (
+              <option key={i.id} value={i.slug}>
+                {i.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      <div className={styles.images}>
+        <label htmlFor="images" className={styles.required}>
+          تصاویر محصول
+        </label>
+
+        {Array.from({ length: imgCounter }).map((_, index) => (
+          <input key={index} type="file" id="images" name="images" accept="image/*" onChange={changeHandler} />
+        ))}
+
+        {form.images.length < 5 && <button onClick={() => setImgCounter(form.images.length + 1)}>افزودن عکس جدید</button>}
+      </div>
+
+      <button className={styles.add} onClick={() => handler(form)}>
+        افزودن محصول
+      </button>
+    </>
+  );
+}
--- pages/profile/add-product.jsx
+++ pages/profile/add-product.jsx
@@ -1,13 +1,32 @@
+import AddProductPage from '@/components/template/AddProductPage';
+import { useAlert } from '@/contexts/AlertProvider';
 import connectDb from '@/utils/connectDb';
 import { verify } from 'jsonwebtoken';
+import { useState } from 'react';
 
 // main page component
 export default function AddProduct({ userId }) {
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  );
+  const showAlert = useAlert();
+
+  function submitFormHandler(form) {
+    if (
+      !form.title.trim() ||
+      !form.description.trim() ||
+      !form.category.trim() ||
+      form.images.length <= 0 ||
+      form.qty <= 0 ||
+      form.price <= 0 ||
+      form.discount < 0 ||
+      form.discount > 100
+    ) {
+      showAlert('error', 'لطفاً تمام فیلدهای الزامی را پر کنید');
+      return;
+    }
+
+    console.log(form);
+  }
+
+  return <AddProductPage handler={submitFormHandler} />;
 }
 
 // redirect and get user id
@@ -22,7 +41,6 @@ export async function getServerSideProps(context) {
       },
     };
   }
-
   try {
     await connectDb();
     const decoded = verify(token, process.env.SECRET_KEY);
